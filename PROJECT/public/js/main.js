6cf1ca313d6abb214e53f6943440b34410afa8a6
Tiago Porto <tiago@tiagoporto.com>
2015-03-08 23:12:03 -0300

    Added analytics click track

--- PROJECT/public/js/main.js
+++ PROJECT/public/js/main.js
@@ -10,13 +10,18 @@ ga('send', 'pageview');
 var CPF = new CPF();
 
 document.getElementById('btn-gerar-CPF').onclick = function(){
+
 	document.getElementById('CPF').innerHTML = CPF.gera();
+
+	ga('send', 'event', 'button', 'click', 'Generate CPF');
 
 };
 
 document.getElementById('valida-CPF').onsubmit = function (event){
 
 	document.getElementById('resultadoValidacao').innerHTML = CPF.valida(document.getElementById('cpf').value);
+
+	ga('send', 'event', 'button', 'click', 'Valide CPF');
 
 	return false;
 
--- PROJECT/src/scripts/demo.js
+++ PROJECT/src/scripts/demo.js
@@ -1,13 +1,18 @@
 var CPF = new CPF();
 
 document.getElementById('btn-gerar-CPF').onclick = function(){
+
 	document.getElementById('CPF').innerHTML = CPF.gera();
+
+	ga('send', 'event', 'button', 'click', 'Generate CPF');
 
 };
 
 document.getElementById('valida-CPF').onsubmit = function (event){
 
 	document.getElementById('resultadoValidacao').innerHTML = CPF.valida(document.getElementById('cpf').value);
+
+	ga('send', 'event', 'button', 'click', 'Valide CPF');
 
 	return false;
 
