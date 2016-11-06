79be27dde33557a298e0db2fee3b69b5a8404440
Tiago Porto <tiago@tiagoporto.com>
2016-11-06 19:05:01 -0200

    Minor changes

--- karma.conf.js
+++ karma.conf.js
@@ -1,16 +1,16 @@
 // Karma configuration
-// Generated on Fri Oct 16 2015 18:47:24 GMT-0300 (E. South America Standard Time)
 
+/* eslint-env node */
+/* eslint strict: ["error", "global"] */
+'use strict';
 module.exports = function(config) {
     var configuration = {
         // base path that will be used to resolve all patterns (eg. files, exclude)
         basePath: '',
 
-
         // frameworks to use
         // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
         frameworks: ['jasmine'],
-
 
         // list of files / patterns to load in the browser
         files: [
@@ -18,30 +18,26 @@ module.exports = function(config) {
             'spec/cpf-spec.js'
         ],
 
-
         // list of files to exclude
         exclude: [
         ],
 
-
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
         preprocessors: {
-            'dist/js/CPF.js' : ['coverage']
+            'dist/js/CPF.js': ['coverage']
         },
 
         // configure the reporter
         coverageReporter: {
-            type : 'html',
-            dir : 'coverage/'
+            type: 'html',
+            dir: 'coverage/'
         },
-
 
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
         reporters: ['progress', 'coverage'],
-
 
         // web server port
         port: 9876,
@@ -50,15 +46,12 @@ module.exports = function(config) {
         // enable / disable colors in the output (reporters and logs)
         colors: true,
 
-
         // level of logging
         // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
         logLevel: config.LOG_INFO,
 
-
         // enable / disable watching file and executing tests whenever any file changes
         autoWatch: true,
-
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
@@ -67,17 +60,15 @@ module.exports = function(config) {
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
         singleRun: false
-
     };
 
-    if(process.env.TRAVIS){
+    if (process.env.TRAVIS) {
         configuration.coverageReporter = {
-            type : 'lcov',
-            dir : 'coverage/'
+            type: 'lcov',
+            dir: 'coverage/'
         },
         configuration.singleRun = true;
     }
-
 
     config.set(configuration);
 };
