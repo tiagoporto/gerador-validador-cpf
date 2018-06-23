a39199f05542dda6cfa4e7254715eed5384122a4
Tiago Porto <tiagoporto@ymail.com>
2018-06-23 18:30:37 -0300

    Add service worker

--- dist/js/vendors.js
+++ dist/js/vendors.js
@@ -20,3 +20,15 @@ addLoadEvent(
 
     $('#validate-section__input--to-format').mask('999.999.999-99')
 );
+
+// This is the "Offline copy of pages" service worker
+
+// Add this below content to your HTML page, or add the js file to your page at the very top to register service worker
+if (navigator.serviceWorker.controller) {
+  console.log('Active service worker found, no need to register')
+} else {
+  // Register the ServiceWorker
+  navigator.serviceWorker.register('service-worker.js', { scope: './' }).then(function (reg) {
+    console.log('Service worker has been registered for scope: ' + reg.scope)
+  })
+}
---
+++ src/scripts/settings/serviceWorker.js
@@ -0,0 +1,11 @@
+// This is the "Offline copy of pages" service worker
+
+// Add this below content to your HTML page, or add the js file to your page at the very top to register service worker
+if (navigator.serviceWorker.controller) {
+  console.log('Active service worker found, no need to register')
+} else {
+  // Register the ServiceWorker
+  navigator.serviceWorker.register('service-worker.js', { scope: './' }).then(function (reg) {
+    console.log('Service worker has been registered for scope: ' + reg.scope)
+  })
+}
