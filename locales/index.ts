4b35b304855ec8521670fdc68f62adbe99bbc807
Tiago Porto <tiagoporto@ymail.com>
2020-05-02 17:57:42 -0300

    Move locales folder

---
+++ locales/index.ts
@@ -0,0 +1,7 @@
+import enResources from './en/app.json'
+import brResources from './br/app.json'
+
+export const i18nResources = {
+  en: enResources,
+  br: brResources,
+}
--- src/site/locales/index.ts
+++
@@ -1,7 +0,0 @@
-import enResources from './en/translations.json'
-import brResources from './br/translations.json'
-
-export const i18nResources = {
-  en: enResources,
-  br: brResources,
-}
