03c81ddf4ef2c5554a533566901325a8b16f5901
Tiago Porto <tiagoporto@ymail.com>
2020-05-02 21:04:54 -0300

    Add i18n

--- src/site/components/ChangeLocale/ChangeLocale.tsx
+++ src/site/components/ChangeLocale/ChangeLocale.tsx
@@ -5,16 +5,36 @@ import styles from './ChangeLocale.module.styl'
 type AvailableLocales = 'en' | 'br'
 
 export const ChangeLocale: FC = () => {
-  const handleLocale = (locale: AvailableLocales) => (): void => {
+  const handleLocale = (
+    locale: AvailableLocales,
+    callback?: () => void
+  ) => (): void => {
     i18next.changeLanguage(locale)
+    window.history.replaceState(
+      {},
+      'Gerador e Validador de CPF',
+      `?lang=${locale}`
+    )
+    callback && setTimeout(callback, 5)
   }
   const [locale, setLocale] = useState('')
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     setLocale(i18next.language)
   }, [i18next.language])
 
-  return (
+  useEffect(() => {
+    const searchParams = new URLSearchParams(window.location.search)
+    const lang = searchParams.get('lang')
+    if (lang) {
+      handleLocale(lang as AvailableLocales, () => setIsLoading(false))()
+    } else {
+      setIsLoading(false)
+    }
+  }, [])
+
+  return isLoading ? null : (
     <div className={styles.box}>
       <button
         className={`${styles.button} ${locale === 'br' ? styles.selected : ''}`}
