603d342718825b1e4715f1452df2aa249ea6dbe8
Tiago Porto <tiagoporto@ymail.com>
2020-05-05 20:20:30 -0300

    Update tests

--- src/lib/CPFGenerate.test.ts
+++ src/lib/CPFGenerate.test.ts
@@ -1,9 +1,27 @@
-import { generate, validate } from './CPF'
-
-const CPF = { generate, validate }
+import * as CPF from './CPF'
+import * as utils from './utils'
 
 describe('Generate function', () => {
   it('generate a valid CPF', () => {
+    expect(CPF.validate(CPF.generate())).toBeTruthy()
+  })
+
+  it('generate a formated CPF', () => {
+    const cpf = CPF.generate({ format: true })
+
+    expect(/^\d{3}.\d{3}.\d{3}-\d{2}$/.test(cpf)).toBeTruthy()
+  })
+
+  it('generate a non formated CPF', () => {
+    const cpf = CPF.generate({ format: false })
+
+    expect(/^\d{11}$/.test(cpf)).toBeTruthy()
+  })
+
+  it('check if is not generatin a cpf with all number equal', () => {
+    const mock = jest.spyOn(utils, 'generateFirstDigits')
+    mock.mockReturnValueOnce('000000000')
+
     expect(CPF.validate(CPF.generate())).toBeTruthy()
   })
 })
