e9be33fbac0b8649208de2ba476cb380ef23f140
Tiago Porto <tiagoporto@ymail.com>
2020-05-23 19:33:32 -0300

    Update unit tests and remove support to validate typeof number

--- src/lib/CPFGenerate.test.ts
+++ src/lib/CPFGenerate.test.ts
@@ -1,24 +1,29 @@
 import * as CPF from './CPF'
 import * as utils from './utils'
 
-describe('Generate function', () => {
-  it('generate a valid CPF', () => {
-    expect(CPF.validate(CPF.generate())).toBeTruthy()
+describe('CPF.generate()', () => {
+  it('Should generate a valid CPF', () => {
+    const cpf = CPF.generate()
+
+    expect(CPF.validate(cpf)).toBeTruthy()
+    expect(isNaN(Number(cpf))).toBeFalsy()
   })
 
-  it('generate a formated CPF', () => {
-    const cpf = CPF.generate({ format: true })
-
-    expect(/^\d{3}.\d{3}.\d{3}-\d{2}$/.test(cpf)).toBeTruthy()
-  })
-
-  it('generate a non formated CPF', () => {
+  it('Should generate a non formated CPF 12345678900', () => {
     const cpf = CPF.generate({ format: false })
 
     expect(/^\d{11}$/.test(cpf)).toBeTruthy()
+    expect(isNaN(Number(cpf))).toBeFalsy()
   })
 
-  it('check if is not generatin a cpf with all number equal', () => {
+  it('Should generate a formated CPF 000.000.000-00', () => {
+    const cpf = CPF.generate({ format: true })
+
+    expect(/^\d{3}.\d{3}.\d{3}-\d{2}$/.test(cpf)).toBeTruthy()
+    expect(isNaN(Number(cpf))).toBeTruthy()
+  })
+
+  it('Should regenerate a new cpf if generate a cpf with all number equals', () => {
     const mock = jest.spyOn(utils, 'generateFirstDigits')
     mock.mockReturnValueOnce('000000000')
 
