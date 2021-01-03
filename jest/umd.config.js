055422dccd86d68698913dd15d00ea065a3e64bd
Tiago Porto <tiagoporto@ymail.com>
2021-01-03 00:11:37 -0300

    Update tests files

--- jest/umd.config.js
+++ jest/umd.config.js
@@ -4,5 +4,12 @@ module.exports = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   moduleNameMapper: {
     './CPF': '<rootDir>/package',
+    '@i18n/(.*)': '<rootDir>/src/site/locales/en/$1',
+    '.+\\.(css|png|jpg|ttf|woff|woff2)$': 'identity-obj-proxy',
+  },
+  transform: {
+    '\\.[jt]sx?$': 'babel-jest',
+    '.+\\.(styl|less|sass|scss)$': 'jest-css-modules-transform',
+    '\\.json$': 'json-map-keys-jest',
   },
 }
