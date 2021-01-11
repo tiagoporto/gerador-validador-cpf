82b55ad499464fb2372b5d095a1e5474c932fdf2
Tiago Porto <tiagoporto@ymail.com>
2021-01-11 11:03:04 -0300

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  collectCoverageFrom: [
+    'src/lib/*.{ts,tsx,js,jsx}'
+  ],
   moduleNameMapper: {
     '@i18n/(.*)': '<rootDir>/src/site/locales/en/$1',
     '.+\\.(css|png|jpg|ttf|woff|woff2)$': 'identity-obj-proxy',
@@ -8,7 +11,6 @@ module.exports = {
   transform: {
     '\\.[jt]sx?$': 'babel-jest',
     '.+\\.(styl|less|sass|scss)$': 'jest-css-modules-transform',
-    '\\.json$': 'json-map-keys-jest',
-    // '\\.json$': '<rootDir>/jsonTransform.js',
+    'src/site/locales/en/.+\\.json$': 'json-map-keys-jest',
   },
 }
