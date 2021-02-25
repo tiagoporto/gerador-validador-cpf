6206be07cda8d384d00a5186bc30194ca697130d
Tiago Porto <tiagoporto@ymail.com>
2021-02-24 22:47:06 -0300

    Fix stryker config

--- stryker.conf.js
+++ stryker.conf.js
@@ -2,6 +2,7 @@
  * @type {import('@stryker-mutator/api/core').StrykerOptions}
  */
 module.exports = {
+  mutate: ['src/lib/*.ts', '!src/lib/*.test.ts'],
   packageManager: 'npm',
   testRunner: 'jest',
   coverageAnalysis: 'perTest',
