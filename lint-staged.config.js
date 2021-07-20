386f4f589fec30d276154d2ae203317631e68319
Tiago Porto <tiago@tiagoporto.com>
2021-07-19 21:54:42 -0300

    Update scripts

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   '*': ['prettier --check --ignore-unknown --write', 'git add'],
   '*.{ts,tsx}': () => 'tsc --project tsconfig.json --noEmit',
+  '*.md': [
+    'remark --frail --use validate-links --use remark-lint-no-dead-urls --use remark-lint-no-url-trailing-slash',
+  ],
   '*.{js,jsx,ts,tsx,md,mdx}': [
     'eslint --ext js,jsx,ts,tsx,md,mdx --max-warnings 0',
     'jest --bail --findRelatedTests',
