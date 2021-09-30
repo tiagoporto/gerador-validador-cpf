5757739be3dd8e3fd6406e2c06e40c50f81851d3
Tiago Porto <tiago@tiagoporto.com>
2021-09-30 20:45:55 -0300

    chore: split lint staged rules

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  '*': ['prettier --check --ignore-unknown --write', 'git add'],
-  '*.{ts,tsx}': () => 'tsc --project tsconfig.json --noEmit',
-  '*.md': [
+  '*': 'prettier --check --ignore-unknown --write',
+  '!(CHANGELOG).md':
     'remark --frail --use validate-links --use remark-lint-no-dead-urls --use remark-lint-no-url-trailing-slash',
-  ],
-  '*.{js,jsx,ts,tsx,md,mdx}': [
-    'eslint --ext js,jsx,ts,tsx,md,mdx --max-warnings 0',
+  '*.{md,mdx}': 'eslint --ext md,mdx --max-warnings 0',
+  '*.{ts,tsx}': () => 'tsc --project tsconfig.json --noEmit',
+  '*.{js,jsx,ts,tsx}': [
+    'eslint --ext js,jsx,ts,tsx --max-warnings 0',
     'jest --bail --findRelatedTests',
   ],
 }
