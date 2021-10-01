21f484c96d8e6a0c0cda2faf45e873e0f7cca1ee
Tiago Porto <tiago@tiagoporto.com>
2021-09-30 21:35:52 -0300

    chore: fix check docs

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   '*': 'prettier --check --ignore-unknown --write',
-  '!(CHANGELOG).md':
-    'remark --frail --use validate-links --use remark-lint-no-dead-urls --use remark-lint-no-url-trailing-slash',
+  '*.md': 'remark --frail',
   '*.{md,mdx}': 'eslint --ext md,mdx --max-warnings 0',
   '*.{ts,tsx}': () => 'tsc --project tsconfig.json --noEmit',
   '*.{js,jsx,ts,tsx}': [
