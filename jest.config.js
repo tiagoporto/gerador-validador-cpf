6ef7c934d53be94373ee7f4d3cbdc8e599192c14
Tiago Porto <tiago@tiagoporto.com>
2024-12-30 23:57:30 -0300

    test: remove unnecessary dependencies and update stryker

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,11 @@
-module.exports = {
+export default {
   roots: ['<rootDir>/src'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   collectCoverageFrom: ['src/lib/*.{ts,tsx,js,jsx}'],
   moduleNameMapper: {
     '@i18n/(.*)': '<rootDir>/src/site/locales/en/$1',
-    '.+\\.(css|png|jpg|ttf|woff|woff2)$': 'identity-obj-proxy',
   },
   transform: {
     '\\.[jt]sx?$': 'babel-jest',
-    '.+\\.(styl|less|sass|scss)$': 'jest-css-modules-transform',
   },
 }
--- jest/es.config.js
+++ jest/es.config.js
@@ -1,14 +1,12 @@
-module.exports = {
+export default {
   rootDir: '../',
   roots: ['<rootDir>/src'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   moduleNameMapper: {
     './CPF': '<rootDir>/dist/CPF.es',
     '@i18n/(.*)': '<rootDir>/src/site/locales/en/$1',
-    '.+\\.(css|png|jpg|ttf|woff|woff2)$': 'identity-obj-proxy',
   },
   transform: {
     '\\.[jt]sx?$': 'babel-jest',
-    '.+\\.(styl|less|sass|scss)$': 'jest-css-modules-transform',
   },
 }
