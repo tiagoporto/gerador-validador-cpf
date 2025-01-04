ddefb6e8f527d8f600a1a15593be747f225a623d
Tiago Porto <tiago@tiagoporto.com>
2025-01-03 23:06:48 -0300

    test: update jest rootDir

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,7 +1,7 @@
 export default {
-  roots: ['<rootDir>/src'],
+  roots: ['<rootDir>'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  collectCoverageFrom: ['src/lib/*.{ts,tsx,js,jsx}'],
+  collectCoverageFrom: ['packages/gerador-validador-cpf/*.{ts,tsx,js,jsx}'],
   transform: {
     '\\.[jt]sx?$': 'babel-jest',
   },
