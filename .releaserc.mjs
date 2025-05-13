115d0d3e927952a69d91732661e0a5268b24608d
Tiago Porto <tiago@tiagoporto.com>
2025-05-13 16:21:32 -0300

    chore: unify semantic release config

---
+++ .releaserc.mjs
@@ -0,0 +1,37 @@
+export default {
+  branches: [
+    'main',
+    {
+      name: 'alpha',
+      channel: 'alpha',
+      prerelease: true,
+    },
+    {
+      name: 'beta',
+      channel: 'beta',
+      prerelease: true,
+    },
+  ],
+  preset: 'conventionalcommits',
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        releaseRules: [
+          {
+            type: 'docs',
+            release: 'patch',
+          },
+          {
+            type: 'refactor',
+            release: 'minor',
+          },
+        ],
+      },
+    ],
+    '@semantic-release/release-notes-generator',
+    '@sebbo2002/semantic-release-jsr',
+    '@anolilab/semantic-release-pnpm',
+    '@semantic-release/github',
+  ],
+}
