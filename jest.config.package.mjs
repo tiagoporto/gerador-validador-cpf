1a374da3e169e5fac2b33bc62a6cd09a3ecddf1b
Tiago Porto <tiago@tiagoporto.com>
2025-05-15 09:16:27 -0300

    test: fix package tests

--- jest.config.package.mjs
+++ jest.config.package.mjs
@@ -4,8 +4,6 @@ import jestConfig from './jest.config.mjs'
 export default {
   ...jestConfig,
   moduleNameMapper: {
-    '^./index.js$': '<rootDir>/packages/gerador-validador-cpf/dist/index.js',
-    '^./utils/generateFirstDigits.js$':
-      '<rootDir>/packages/gerador-validador-cpf/dist/utils/generateFirstDigits.js',
+    '^./index.js$': '../package/dist/index.js',
   },
 }
