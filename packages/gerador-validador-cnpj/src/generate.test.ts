0d27f45510a4d626cc58df1753135fd36a562323
Tiago Porto <tiago@tiagoporto.com>
2025-05-15 15:59:52 -0300

    test(cnpj): add generate tests

--- packages/gerador-validador-cnpj/src/generate.test.ts
+++ packages/gerador-validador-cnpj/src/generate.test.ts
@@ -9,14 +9,14 @@ describe('generate cnpj', () => {
       mock: [0.1, 0.22, 0, 0.22, 0.1, 0.05, 0.13, 0, 0, 0, 0, 0.03],
       expected: '49094250000127',
     },
-    // {
-    //   mock: [0.9, 0.2, 0.6, 0, 0.3, 0.8, 0.9, 0.9, 0],
-    //   expected: '92603899090',
-    // },
-    // {
-    //   mock: [0.6, 0.3, 0.6, 0.1, 0.5, 0, 0.6, 0.4, 0],
-    //   expected: '63615064011',
-    // },
+    {
+      mock: [0.13, 0.07, 0, 0.08, 0, 0.2, 0.14, 0.15, 0, 0, 0, 0.04],
+      expected: '53030866000101',
+    },
+    {
+      mock: [0.07, 0.19, 0.11, 0.18, 0.05, 0.14, 0.17, 0.18, 0, 0, 0, 0.04],
+      expected: '38472677000103',
+    },
   ])('should generate a valid CNPJ $expected', ({ mock, expected }) => {
     mockRandom(mock)
     const cnpj = generate()
@@ -24,64 +24,40 @@ describe('generate cnpj', () => {
     expect(cnpj).toBe(expected)
   })
 
-  // eslint-disable-next-line jest/no-commented-out-tests
-  // it.each([
-  //   {
-  //     mock: [0.5, 0.3, 0.1, 0.4, 0.7, 0.4, 0, 0, 0],
-  //     expected: '53147400018',
-  //   },
-  //   {
-  //     mock: [0.6, 0.9, 0, 0.4, 0, 0.7, 0.2, 0.3, 0],
-  //     expected: '69040723001',
-  //   },
-  // ])(
-  //   'should generate a valid non formatted cnpj $expected',
-  //   ({ mock, expected }) => {
-  //     mockRandom(mock)
-  //     const cnpj = generate({ format: false })
+  it.each([
+    {
+      mock: [0.04, 0.21, 0.15, 0, 0.05, 0.19, 0, 0.22, 0, 0, 0],
+      expected: '19602809000111',
+    },
+    {
+      mock: [0.08, 0.1, 0.11, 0.2, 0.21, 0.06, 0.07, 0.22, 0, 0, 0, 0.03],
+      expected: '34489239000199',
+    },
+  ])(
+    'should generate a valid non formatted cnpj $expected',
+    ({ mock, expected }) => {
+      mockRandom(mock)
+      const cnpj = generate({ format: false })
 
-  //     expect(cnpj).toBe(expected)
-  //     expect(Number.isNaN(Number(cnpj))).toBeFalsy()
-  //   },
-  // )
+      expect(cnpj).toBe(expected)
+      expect(Number.isNaN(Number(cnpj))).toBeFalsy()
+    },
+  )
 
-  // eslint-disable-next-line jest/no-commented-out-tests
-  // it.each([
-  //   {
-  //     mock: [0.6, 0.4, 0.5, 0.1, 0.5, 0.2, 0.9, 0.3, 0],
-  //     expected: '645.152.930-36',
-  //   },
-  //   {
-  //     mock: [0.7, 0.8, 0.6, 0, 0.6, 0.3, 0.7, 0.7, 0],
-  //     expected: '786.063.770-74',
-  //   },
-  // ])('should generate a formatted cnpj $expected', ({ mock, expected }) => {
-  //   mockRandom(mock)
-  //   const cnpj = generate({ format: true })
+  it.each([
+    {
+      mock: [0.06, 0, 0.15, 0.12, 0.04, 0.13, 0.06, 0.05, 0, 0, 0, 0.03],
+      expected: '20.651.522/0001-60',
+    },
+    {
+      mock: [0.17, 0.06, 0, 0.1, 0.11, 0.18, 0.05, 0.18, 0, 0, 0, 0.03],
+      expected: '72.044.727/0001-81',
+    },
+  ])('should generate a formatted cnpj $expected', ({ mock, expected }) => {
+    mockRandom(mock)
+    const cnpj = generate({ format: true })
 
-  //   expect(cnpj).toBe(expected)
-  //   expect(Number.isNaN(Number(cnpj))).toBeTruthy()
-  // })
-
-  // eslint-disable-next-line jest/no-commented-out-tests
-  // it.each([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])(
-  //   'should regenerate a new cnpj if all digits are %i',
-  //   (value) => {
-  //     const number = Number(`0.${value}`)
-  //     jest
-  //       .spyOn(Math, 'random')
-  //       .mockReturnValueOnce(number)
-  //       .mockReturnValueOnce(number)
-  //       .mockReturnValueOnce(number)
-  //       .mockReturnValueOnce(number)
-  //       .mockReturnValueOnce(number)
-  //       .mockReturnValueOnce(number)
-  //       .mockReturnValueOnce(number)
-  //       .mockReturnValueOnce(number)
-  //       .mockReturnValueOnce(number)
-  //     const cnpj = generate()
-
-  //     expect(cnpj).not.toBe(value.toString().repeat(11))
-  //   },
-  // )
+    expect(cnpj).toBe(expected)
+    expect(Number.isNaN(Number(cnpj))).toBeTruthy()
+  })
 })
