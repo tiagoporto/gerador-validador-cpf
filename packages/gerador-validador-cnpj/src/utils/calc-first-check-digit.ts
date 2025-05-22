37d9de34cd4a4cbb0fef6ce9495bd4b97c2e3afe
Pedro Henrique Rodrigues <pedrohrd87@gmail.com>
2025-05-22 17:54:05 +0000

    refactor: optimize calcFirstCheckDigit to reduce complexity and improve performance

--- packages/gerador-validador-cnpj/src/utils/calc-first-check-digit.ts
+++ packages/gerador-validador-cnpj/src/utils/calc-first-check-digit.ts
@@ -1,18 +1,12 @@
 export const calcFirstCheckDigit = (firstTwelveDigits: string) => {
   const importanceArray = [5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
 
-  const sum = firstTwelveDigits
-    // eslint-disable-next-line unicorn/prefer-spread
-    .split('')
-    .map((item, index) => {
-      // get ascii code
-      const value = (item.codePointAt(0) ?? -1) - 48
-      const importance = importanceArray[index]
-      return value * importance
-    })
-    .reduce((previous, current) => previous + current, 0)
+  let sum = 0
+  for (let i = 0; i < 12; i++) {
+    const digit = (firstTwelveDigits.codePointAt(i) ?? 0) - 48
+    sum += digit * importanceArray[i]
+  }
 
   const remainder = sum % 11
-
   return remainder < 2 ? 0 : 11 - remainder
 }
