d2ea0b15a21129752c0f5ccc312d29b3d8cecc6b
Tiago Porto <tiago@tiagoporto.com>
2025-09-15 11:11:25 -0300

    feat(cnpj)!: remove validateAlphanumeric compatibility

--- packages/gerador-validador-cnpj/src/validate.ts
+++ packages/gerador-validador-cnpj/src/validate.ts
@@ -3,21 +3,16 @@ import { calcCheckDigits, isAlphanumericCNPJ } from './utils/index.js'
 /**
  * Validates a given CNPJ (Cadastro Nacional da Pessoa Jurídica) number.
  * @param value  - The CNPJ number as a string
- * @param options - options for CNPJ validation
- * @param options.validateAlphanumeric - If true, will validate an alphanumeric CNPJ
  * @returns      true = valid || false = invalid
  */
-export const validate = (
-  value: string,
-  { validateAlphanumeric = false }: { validateAlphanumeric?: boolean } = {},
-): boolean => {
+export const validate = (value: string): boolean => {
   const NOT_ALLOWED_SYMBOLS = /[^A-Z\d./-]/i
 
   if (typeof value !== 'string' || NOT_ALLOWED_SYMBOLS.test(value)) {
     return false
   }
 
-  const replaceRegex = validateAlphanumeric ? /[./-]/g : /[A-Z./-]/g
+  const replaceRegex = /[./-]/g
   const cleanCNPJ = value.replaceAll(replaceRegex, '')
 
   if (!isAlphanumericCNPJ(cleanCNPJ) || cleanCNPJ === '00000000000000') {
